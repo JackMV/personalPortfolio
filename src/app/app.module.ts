051576cb219af066379f41739a9673dd2d9a14b2
JackMV <jack.mccabev@gmail.com>
2019-03-22 08:26:41 -0500

    Added basic components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './content/navbar/navbar.component';
+import { HomeComponent } from './content/home/home.component';
+import { WorkComponent } from './content/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
